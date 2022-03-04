dbe60a85b9019476e252bbcfe9a1645be55c60db
Lesanojanelle <100949458+Lesanojanelle@users.noreply.github.com>
2022-03-04 23:53:05 +0800

    Add files via upload
    MyCandyMachine Activity#2

---
+++ inbound7036217353822968543.java
@@ -0,0 +1,36 @@
+
+ MyCandyMachine!
+  
+  
+    } public static void main (string[]args)       {
+ 
+ "MyCandyMachine"
+
+"[Class flattops 35]
+
+"[Class Mentos 80]
+
+"[Class gummy 32]
+
+"[class Icool 50]
+
+"[class Max 60]
+
+ what do you want to buy?
+ "flattops
+
+
+"how many of flottops?
+}
+1
+
+"Pay your bills"
+35
+
+"this is your items"!
+"thank You so much"
+
+
+
+
+" THank you and your welcome"!
